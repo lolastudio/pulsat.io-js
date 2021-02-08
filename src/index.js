ca50eef5b4dccab7a1c0d9be1f9b23b7092f373d
Roque Francisco <roquefsd@gmail.com>
2021-02-08 10:17:13 -0300

    bump v1.2.0

--- src/index.js
+++ src/index.js
@@ -73,9 +73,7 @@ class Pulsatio {
             response.json().then((body) => {
                 this.last_message_id = (body || {})._message_id;
                 if (this.options.on.heartbeat) { this.options.on.heartbeat(body); }
-            }).catch(err => {
-                console.log(err);
-            })
+            }).catch(err => { })
 
             if (response && response.status !== 404) {
                 this.disconnected = null
